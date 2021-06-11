122cbebe81a05745f5083a723bcf97e5d4f3f4bc
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-06-11 08:17:25 +0200

    feat(compression): add quantization + test on wikipedia matrix

--- src/compression.py
+++ src/compression.py
@@ -1,5 +1,16 @@
 import numpy as np
-from numpy.core.fromnumeric import compress
+import scipy as sp
+
+Q = np.array([
+    [16, 11, 10, 16, 24, 40, 51, 61],
+    [12, 12, 14, 19, 26, 58, 60, 55],
+    [14, 13, 16, 24, 40, 57, 69, 56],
+    [14, 17, 22, 29, 51, 87, 80, 62],
+    [18, 22, 37, 56, 68, 109, 103, 77],
+    [24, 35, 55, 64, 81, 104, 113, 92],
+    [49, 64, 78, 87, 103, 121, 120, 101],
+    [72,  92, 95, 98, 112, 100, 103, 99]]
+)
 
 def padding(img, mode="black"):
     """
@@ -44,22 +55,54 @@ def block_splitting(img):
         for j in range(0, img.shape[1], 8):
             yield img[i:i+8, j:j+8]
 
+def dct(block):
+    block =  block - 128
+    dct_block = sp.fft.dct(block, axis=0, type=2, norm="ortho")
+    dct_block = sp.fft.dct(dct_block, axis=1, type=2, norm="ortho")
+    return dct_block
+
+def quantization(dct_block, Q):
+    return np.rint(np.divide(dct_block, Q)).astype(int)
+
 def compression(img):
     if len(img.shape) != 3 or img.shape[2] != 3:
         raise ValueError("Input image dimension is not supported")
 
     img = np.transpose(img, (2, 0, 1))
     
+    img_compressed = []
     for channel in range(3):
         # Step 1: Block splitting
         img_channel = padding(img[channel, ...])
-        print(img_channel.shape)
-        for macro_block in block_splitting(img_channel):
-            print(f"channel {channel}: {macro_block.shape}")
-        # Step 2: DCT
-        # Step 3: Quantization
-        # Step 4: Zigzag + Huffman
+
+        for block in block_splitting(img_channel):
+            # Step 2: Discrete cosine transform (DCT)
+            dct_block = dct(block)
+            # Step 3: Quantization + Round to nearest integer
+            q_block = quantization(dct_block, Q)
+            # Step 4: Zigzag + Huffman
+            raise Exception("")
+
+            img_compressed.append(block)
+    
+    return img_compressed
+
+# dummy = np.ones((11, 11, 3))
+# compression(dummy)
+
+# Taken from wikipedia: https://en.wikipedia.org/wiki/JPEG#Encoding
+block = np.array([
+    [52, 55, 61, 66, 70, 61, 64, 73],
+    [63, 59, 55, 90, 109, 85, 69, 72],
+    [62, 59, 68, 113, 144, 104, 66, 73],
+    [63, 58, 71, 122, 154, 106, 70, 69],
+    [67, 61, 68, 104, 126, 88, 68, 70],
+    [79, 65, 60, 70, 77, 68, 58, 75],
+    [85, 71, 64, 59, 55, 61, 65, 83],
+    [87,  79, 69, 68, 65, 76, 78, 94]]
+)
+
+def f():
     pass
 
-# dummy = np.ones((11, 5, 3))
-# compression(dummy)
+f()
--- test/test_compression.py
+++ test/test_compression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from src.compression import padding
+from src.compression import *
 
 class TestPadding:
 
@@ -60,3 +60,83 @@ class TestPadding:
         for mode in ["black", "replicate"]:
             new_img = padding(img, mode=mode)
             assert new_img.shape == (8, 16)
+
+class TestDct:
+    @classmethod
+    def setup_class(cls):
+        # Taken from wikipedia: https://en.wikipedia.org/wiki/JPEG#Encoding
+        cls.block = np.array([
+            [52, 55, 61, 66, 70, 61, 64, 73],
+            [63, 59, 55, 90, 109, 85, 69, 72],
+            [62, 59, 68, 113, 144, 104, 66, 73],
+            [63, 58, 71, 122, 154, 106, 70, 69],
+            [67, 61, 68, 104, 126, 88, 68, 70],
+            [79, 65, 60, 70, 77, 68, 58, 75],
+            [85, 71, 64, 59, 55, 61, 65, 83],
+            [87,  79, 69, 68, 65, 76, 78, 94]]
+        )
+
+        cls.expected = np.array([
+            [-415.38, -30.19, -61.20, 27.24, 56.12, -20.10, -2.39, 0.46],
+            [4.47, -21.86, -60.76, 10.25, 13.15, -7.09, -8.54, 4.88],
+            [-46.83, 7.37, 77.13, -24.56, -28.91, 9.93, 5.42, -5.65],
+            [-48.53, 12.07, 34.10, -14.76, -10.24, 6.30, 1.83, 1.95],
+            [12.12, -6.55, -13.20, -3.95, -1.87, 1.75, -2.79, 3.14],
+            [-7.73, 2.91, 2.38, -5.94, -2.38, 0.94, 4.30, 1.85],
+            [-1.03, 0.18, 0.42, -2.42, -0.88, -3.02, 4.12, -0.66],
+            [-0.17, 0.14, -1.07, -4.19, -1.17, -0.10, 0.50, 1.68]]
+        )
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_dct(self):
+        result = dct(self.block)
+        np.allclose(result, self.expected)
+
+class TestQuantization:
+    @classmethod
+    def setup_class(cls):
+        # Taken from wikipedia: https://en.wikipedia.org/wiki/JPEG#Encoding
+        cls.dct_block = np.array([
+            [-415.38, -30.19, -61.20, 27.24, 56.12, -20.10, -2.39, 0.46],
+            [4.47, -21.86, -60.76, 10.25, 13.15, -7.09, -8.54, 4.88],
+            [-46.83, 7.37, 77.13, -24.56, -28.91, 9.93, 5.42, -5.65],
+            [-48.53, 12.07, 34.10, -14.76, -10.24, 6.30, 1.83, 1.95],
+            [12.12, -6.55, -13.20, -3.95, -1.87, 1.75, -2.79, 3.14],
+            [-7.73, 2.91, 2.38, -5.94, -2.38, 0.94, 4.30, 1.85],
+            [-1.03, 0.18, 0.42, -2.42, -0.88, -3.02, 4.12, -0.66],
+            [-0.17, 0.14, -1.07, -4.19, -1.17, -0.10, 0.50, 1.68]]
+        )
+        
+        cls.Q = np.array([
+            [16, 11, 10, 16, 24, 40, 51, 61],
+            [12, 12, 14, 19, 26, 58, 60, 55],
+            [14, 13, 16, 24, 40, 57, 69, 56],
+            [14, 17, 22, 29, 51, 87, 80, 62],
+            [18, 22, 37, 56, 68, 109, 103, 77],
+            [24, 35, 55, 64, 81, 104, 113, 92],
+            [49, 64, 78, 87, 103, 121, 120, 101],
+            [72,  92, 95, 98, 112, 100, 103, 99]]
+        )
+        
+        cls.expected = np.array([
+            [-26, -3, -6, 2, 2, -1, 0, 0],
+            [0, -2, -4, 1, 1, 0, 0, 0],
+            [-3, 1, 5, -1, -1, 0, 0, 0],
+            [-3, 1, 2, -1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]]
+        )
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_quantization(self):
+        result = quantization(self.dct_block, self.Q)
+        np.allclose(result, self.expected)
+        assert result.dtype == self.expected.dtype
