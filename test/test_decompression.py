1be16bb4979b1c6e7086fd3656cd1eee7f220ed5
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-06-12 17:22:15 +0200

    feat(decompression): add dct_inv + test

--- test/test_decompression.py
+++ test/test_decompression.py
@@ -104,3 +104,38 @@ class TestQuantizationInv:
     def test_quantization_inv(self):
         result = quantization_inv(self.q_block, Q_MAT)
         np.allclose(result, self.expected)
+
+class TestDctInv:
+
+    @classmethod
+    def setup_class(cls):
+
+        cls.dct_block = np.array([
+            [-416,  -33,  -60,   32,   48,  -40,    0,    0],
+            [   0,  -24,  -56,   19,   26,    0,    0,    0],
+            [ -42,   13,   80,  -24,  -40,    0,    0,    0],
+            [ -42,   17,   44,  -29,    0,    0,    0,    0],
+            [  18,    0,    0,    0,    0,    0,    0,    0],
+            [   0,    0,    0,    0,    0,    0,    0,    0],
+            [   0,    0,    0,    0,    0,    0,    0,    0],
+            [   0,    0,    0,    0,    0,    0,    0,    0]
+        ])
+
+        cls.expected = np.array([
+            [62, 65, 57, 60, 72, 63, 60, 82],
+            [57, 55, 56, 82, 108, 87, 62, 71],
+            [58, 50, 60, 111, 148, 114, 67, 65],
+            [65, 55, 66, 120, 155, 114, 68, 70],
+            [70, 63, 67, 101, 122, 88, 60, 78],
+            [71, 71, 64, 70, 80, 62, 56, 81],
+            [75, 82, 67, 54, 63, 65, 66, 83],
+            [81, 94, 75, 54, 68, 81, 81, 8]
+        ])
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def test_dct_inv(self):
+        result = dct_inv(self.dct_block)
+        np.allclose(result, self.expected)
