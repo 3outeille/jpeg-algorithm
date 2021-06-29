05f4a14c1c129edaa2d43c23ef58e8226d4c7518
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-06-29 21:48:10 +0200

    chore(.sh): change shebang to sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 pip install virtualenv
 virtualenv codo-env
-source codo-env/bin/activate
+. codo-env/bin/activate
 pip install -r requirements.txt
 python setup.py clean
 python -m ipykernel install --user --name=codo-env
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 jupyter kernelspec uninstall codo-env
 rm -rf codo-env
