60149a76da736ff4eb0e4c5d27cf0e32fee8fcc9
Ferdinand Mom <ferdinand.mom@epita.fr>
2021-06-29 21:10:11 +0200

    feat(jpeg-algorithm): add code

--- src/decompression.py
+++ src/decompression.py
@@ -160,29 +160,6 @@ def unpadding(img, info_padding):
 
     return og_img
 
-def yuv2rgb(img):
-    """
-        Convert the YUV channels into RGB channels
-
-        @Params:
-        - img: input image of shape (n, m, c).
-    """
-    n, m, c = img.shape
-    R = np.zeros((n, m))
-    G = np.zeros((n, m))
-    B = np.zeros((n, m))
-    for i in range(n):
-        for j in range(m):
-            R[i][j] = 1 * img[...,0][i][j] + 1.13983 * img[...,2][i][j]
-            G[i][j] = 1 * img[...,0][i][j] - 0.39465 * img[...,1][i][j] - 0.58060 * img[...,2][i][j]
-            B[i][j] = 1 * img[...,0][i][j] + 2.03211 * img[...,1][i][j]
-
-    img[...,0] = R
-    img[...,1] = G
-    img[...,2] = B
-
-    return img
-
 def upsampling(img, downsampling_mode):
     """
         Upsampling the channel U and V
